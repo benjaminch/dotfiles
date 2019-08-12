6bdd3a41e569f7f02852d191c838a8421907a4eb
Benjamin <b.chastanier@criteo.com>
2019-08-12 20:21:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@
 # https://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Make sure clipboard works in TMUX / VIM
+brew install reattach-to-user-namespace
