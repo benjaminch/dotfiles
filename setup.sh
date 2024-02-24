7330b403631fc7509bb19e752ff4c15ef97a0888
BenjaminCh <bchastanier@qovery.com>
2024-02-24 08:11:58 +0100

    Update install script

--- setup.sh
+++ setup.sh
@@ -4,5 +4,70 @@
 # https://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# TERMINAL
+
+# Install WezTerm
+cp .wezterm.lua ~/.wezterm.lua 
+brew install --cask wezterm
+
+# Install tmux
+cp .tmux ~/
+brew install tmux
+
 # Make sure clipboard works in TMUX / VIM
 brew install reattach-to-user-namespace
+
+# Install better command history (atuin)
+cp ./.config/atuin ~/.config/atuin
+brew install atuin
+
+
+
+# TILING MANAGER
+
+# Install Yabai (tiling manager)
+cp ./.config/yabai ~/.config/yabai
+brew install koekeishiya/formulae/yabai
+
+# Install skhd (keyboard shortcut manager)
+cp ./.config/skhd ~/.config/skhd
+brew install koekeishiya/formulae/skhd
+
+
+
+# TEXT EDITOR
+
+# Neovim
+cp ./.config/nvim ~/.config/nvim
+brew install neovim
+
+
+
+# DEV
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Go
+brew install go
+
+
+
+# OPS
+
+# Install Docker
+brew install docker
+
+# Install Terraform
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl"
+
+# Install Kubernetes management tool (k9s)
+cp ./.config/k9s ~/.config/k9s
+brew install derailed/k9s/k9s
+
+# Install kubectl extensions (kubectl view allocation)
+cargo install kubectl-view-allocations
