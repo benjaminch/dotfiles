27f4e5d9b1ddfbbd7dfbcc7fc2c0426a1e0f9b9d
BenjaminCh <bchastanier@qovery.com>
2024-02-24 08:08:24 +0100

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Peppermint'
+
+config.font = wezterm.font 'Fira Code'
+config.font =
+  wezterm.font('JetBrains Mono', { weight = 'Bold', italic = false })
+
+
+return config
