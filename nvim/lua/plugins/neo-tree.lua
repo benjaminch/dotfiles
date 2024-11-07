3763ca089f36c384a694bd4cb06f3da349b5dcda
bchastanier <bchastanier@qovery.com>
2024-11-07 10:05:04 +0100

    chore(nvim): update config

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      hide_dotfiles = false,
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+}
